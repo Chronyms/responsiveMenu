3508c69c78a47c4145e3b8c75fdfc25172bdbff6
Caspar Brenneisen <caspar.brenneisen@hotmail.ch>
2016-08-01 12:10:52 +0200

    Added commentarys
    Added commentarys

--- index.php
+++ index.php
@@ -11,43 +11,69 @@
   * And btw., please do crazy stuff.
   * -----------------------------------------------------------------------------------
 -->
-
+<!-- Defines the file as HTML-file -->
 <!DOCTYPE html>
+<!-- Open the HTML-section and defines the language as german -->
 <html lang="de">
 	<head>
+		<!-- Defines the charset as UTF 8 -->
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- Defines the viewport which is required for the responsive design -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
+		<!-- Defines the title of the page -->
 		<title>Dropdown Menu Test</title>
 		
+		<!-- Links the main css-file to the page -->
 		<link rel="stylesheet" type="text/css" href="css/mainCss.css" />
 	</head>
 	<body>
+		<!-- Creates a container for the navigation -->
 		<nav>
+			<!-- Creates a unordered list in the nav -->
 			<ul>
+				<!-- Creates a unordered list-point with a local link -->
 				<li><a href="#"><p>Home</p></a></li>
+				<!-- Creates a unordered list-point with a local link -->
 				<li><a href="#"><p>Punkt 1</p></a>
+					<!-- Creates a unordered list in an unordered list -->
 					<ul>
+						<!-- Creates a unordered list-point with a local link -->
 						<li><a href="#"><p>Unterpunkt 1</p></p></a></li>
+						<!-- Creates a unordered list-point with a local link -->
 						<li><a href="#"><p>Unterpunkt 2</p></a></li>
+						<!-- Creates a unordered list-point with a local link -->
 						<li><a href="#"><p>Unterpunkt 3</p></a></li>
+					<!-- Close the unordered list in the unordered list -->
 					</ul>
 				</li>
+				<!-- Creates a unordered list-point with a local link -->
 				<li><a href="#"><p>Punkt 2</p></a></li>
+				<!-- Creates a unordered list-point with a local link -->
 				<li><a href="#"><p>Punkt 3</p></a>
+					<!-- Creates a unordered list in an unordered list -->
 					<ul>
+						<!-- Creates a unordered list-point with a local link -->
 						<li><a href="#"><p>Unterpunkt 1</p></a></li>
+						<!-- Creates a unordered list-point with a local link -->
 						<li><a href="#"><p>Unterpunkt 2</p></a></li>
+						<!-- Close the unordered list in the unordered list -->
 					</ul>
 				</li>
+				<!-- Creates a unordered list-point with a local link -->
 				<li><a href="#"><p>Punkt 4</p></a></li>
+				<!-- Close the unordered list -->
 			</ul>
+		<!-- Close the navigation container -->
 		</nav>
-		<!---->
+		
+		<!--Add the jQuary framework -->
 		<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+		<!--Add the hoverIntent plugin -->
 		<script type="text/javascript" src="js/hoverIntent.js"></script>
+		<!--Add the detectMobileBrowser plugin -->
 		<script type="text/javascript" src="js/detectMobileBrowser.js"></script>
+		<!--Add the main javascript -->
 		<script type="text/javascript" src="js/mainJs.js"></script>
 	</body>
 </html>
--- js/mainJs.js
+++ js/mainJs.js
@@ -12,37 +12,53 @@
  * -----------------------------------------------------------------------------------
  */
 
+ // Execute the script while the page loads
 $(document).ready(function(){
+	// Check for mobile browsers
 	if(jQuery.browser.mobile)
 	{
+		// If mobile, return "Mobile"
 		console.log("Mobile");
+		// Execute mobile dropdown
 		clickDropDown();
 	}
 	else
 	{
+		// If not mobile, return "Desktop"
 		console.log("Desktop");
+		// Execute Desktop dropdown
 		hoverDropDown();
 	}
+	// Add class "js" to "nav"-container
 	$('nav').addClass('js');
 });
 
+// Desktop dropdown function
 function hoverDropDown()
 {
+	// Detect hovers of the curser
 	$("nav ul li").hoverIntent(function(e)
 	{
+		// Slide down the next "ul"
 		$(this).find('>ul').slideDown('fast');
 	},
+	// After hover
 	function(e)
 	{
+		// Slide up the next "ul"
 		$(this).find('ul').slideUp('fast');
 	});
 };
 
+// Mobile dropdown function
 function clickDropDown()
 {
+	// If click on "li"
 	$("li").click(function()
 	{
+		// Slide Up exept the children of ul
 		$('li > ul').not($(this).children("ul")).slideUp();
+		// Show / Hide the dropdown
         $(this).children("ul").slideToggle();
 	});
 };
